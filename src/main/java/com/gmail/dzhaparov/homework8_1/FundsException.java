bd7a18395d98af327ca4b1b5c4b198ee621722b7
Talvin22 <talvindzaparov1@gmail.com>
2024-09-13 21:37:02 +0200

    Package: homework8_1 added

---
+++ src/main/java/com/gmail/dzhaparov/homework8_1/FundsException.java
@@ -0,0 +1,9 @@
+package com.gmail.dzhaparov.homework8_1;
+
+// Кастомний виняток
+public class FundsException extends Exception {
+
+    public FundsException(String message) {
+        super(message);
+    }
+}
