b3ca77f385e7698844dd65e685dfc56c977f5d5b
Talvin Dzhaparov <talvindzaparov379@gmail.com>
2024-10-15 21:22:54 +0200

    package: homework 15_1 added

---
+++ src/main/java/com/gmail/dzhaparov/homework15_1/DataRepository.java
@@ -0,0 +1,19 @@
+package com.gmail.dzhaparov.homework15_1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DataRepository {
+
+    public Map<Integer, String> getData() {
+        Map<Integer,String > map = new HashMap<>();
+
+        map.put(387, "Lucy");
+        map.put(231, "Alice");
+        map.put(394, "Bob");
+        map.put(172, "Tom");
+
+
+        return map;
+    }
+}
