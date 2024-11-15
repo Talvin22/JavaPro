61bc0fa592679e6489ea042279a43596708ed23a
Talvin Dzhaparov <talvindzaparov379@gmail.com>
2024-11-15 10:31:07 +0100

    package: homework20_1 added

---
+++ src/main/java/com/gmail/dzhaparov/homework20_1/GraphDemo.java
@@ -0,0 +1,31 @@
+package com.gmail.dzhaparov.homework20_1;
+
+public class GraphDemo {
+    public static void main(String[] args) {
+        Graph graph = new Graph();
+
+
+        graph.addVertex(1);
+        graph.addVertex(2);
+        graph.addVertex(3);
+        graph.addEdge(1, 2);
+        graph.addEdge(2, 3);
+
+
+        System.out.println("Граф після додавання вершин и ребер:");
+        graph.printGraph();
+
+
+        System.out.println("\nЧи існує вершина 2? " + graph.hasVertex(2));
+        System.out.println("Чи існує ребро між 1 и 3? " + graph.hasEdge(1, 3));
+
+
+        graph.removeEdge(1, 2);
+        System.out.println("\nГраф після виделення ребра між 1 и 2:");
+        graph.printGraph();
+
+        graph.removeVertex(2);
+        System.out.println("\nГраф після видалення вершини 2:");
+        graph.printGraph();
+    }
+}
